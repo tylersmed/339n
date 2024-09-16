93b8aec584ff4b0ce86c267088c65c47da51b39c
Tyler Smedley <tylersmedley17@gmail.com>
2024-09-16 16:15:09 -0500

    code to fill out score matrix in hw3

--- hw/assignment3.R
+++ hw/assignment3.R
@@ -16,7 +16,24 @@
 ##The score 18 isn't necessarily right with the above given needleman_wunch parameters
 
 global_align = function(seq1, seq2, match_score = 3, mismatch_score = -10, gap_open = -3) {
-  score_matrix = matrix(-Inf, nrow = nchar(seq1) + 1, ncol = nchar(seq2) + 1)
+  m = nchar(seq1)
+  n = nchar(seq2)
+  score_matrix = matrix(-Inf, nrow = m+1, ncol = n+1)
+  # fill out the matrix
+  for (i in 1:(m+1)) {
+    for (j in 1:(n+1)) {
+      if (i == 1) {
+        score_matrix[i,j] = gap_open * j
+      } else if (j == 1) {
+        score_matrix[i, j] = gap_open * i
+      } else {
+        match = score_matrix[i-1, j-1] + ifelse(substr(seq1, i-1, i-1) == substr(seq2, j-1, j-1), match_score, mismatch_score)
+        gap_right = score_matrix[i, j-1] + gap_open
+        gap_down = score_matrix[i-1, j] + gap_open
+        score_matrix[i, j] = max(match, gap_right, gap_down)
+      }
+    }
+  }
   print(score_matrix)
 }
 
